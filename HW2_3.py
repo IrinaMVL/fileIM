a516c1e01270d7e5104f7bcd23cdaa4ae771da71
IrinaMVL <irina.lavysh@mail.ru>
2023-04-03 20:19:22 +0300

    add new fail1 add new fail2 add new fail3

---
+++ HW2_3.py
@@ -0,0 +1,15 @@
+# name = 'Irina'
+# age = '32'
+# city = 'Minsk'
+# result = f'Hello, {name}. Your age: {age}. You are from {city}'
+# print(result)
+
+# name = 'Irina'
+# age = '32'
+# city = 'Minsk'
+# print("Hello " + name + ". Your age: " + age + ". You are from " + city)
+
+name = 'Irina'
+age = '32'
+city = 'Minsk'
+print("Hello, {}. Your age: {}. You are from {}".format(name, age, city))
