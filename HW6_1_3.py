f3e8b34fce304ce9c7313b0f87bfc69f56c888cc
IrinaMVL <irina.lavysh@mail.ru>
2023-04-17 13:49:39 +0300

    add print

---
+++ HW6_1_3.py
@@ -0,0 +1,9 @@
+# numbers = [1, 2, 3, 4, 5, 6, 7]
+# N = 2
+# list = numbers[:5]
+# list2 = numbers[-2:]
+# print(list2 + list)
+
+# numbers = [1, 2, 3, 4, 5, 6, 7]
+# N = 2
+# print(numbers[-N:] + numbers[:5])
