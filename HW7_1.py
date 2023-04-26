f53edc10eaa212a28cc3df65d1ab8c0b0461cc6d
IrinaMVL <irina.lavysh@mail.ru>
2023-04-26 20:59:08 +0300

    changes

--- HW7_1.py
+++ HW7_1.py
@@ -1,13 +1,16 @@
 class Car:
 
-    def __init__(self, blue, 4, is_baby_seat, is_busy):
-        self.color = blue
-        self.count_passenger_seats = 4
+    def __init__(self, color, count_passenger_seats, is_baby_seat, is_busy):
+        self.color: str = 'blue'
+        self.count_passenger_seats: int = 4
         self.is_baby_seat = None
         self.is_busy = False
 
+    def __str__(self):
+        return f""
 
-Toyota = Car('Toyota', 'blue')
+
+Toyota = Car('Toyota', 'blue', 4, None)
 print(Toyota)
 
 
