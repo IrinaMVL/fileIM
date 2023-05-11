dadacbbcc6e5214249e989ea2f0193b10aa9376b
IrinaMVL <irina.lavysh@mail.ru>
2023-05-11 11:15:43 +0300

    add changes

--- HW11_1.py
+++ HW11_1.py
@@ -5,12 +5,15 @@ class Car(BaseModel):
     name: str
     model: str
     engine: float
+    owner_phonenumber: str
 
 
 data = {
     'name': 'Toyota',
     'model': 'Rav4',
-    'engine': 2.4
+    'engine': 2.4,
+    'owner_phonenumber': '+375295711010'
 }
 car = Car(**data)
+print(car)
 
